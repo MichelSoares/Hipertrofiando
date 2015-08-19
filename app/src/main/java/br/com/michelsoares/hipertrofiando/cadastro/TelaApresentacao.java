f24109117763c47681a47c33a0300387d327c71b
MichelDesktop <misoares.777@gmail.com>
2015-08-18 22:57:10 -0300

    Iniciando Porjeto Hipertrofiando

---
+++ app/src/main/java/br/com/michelsoares/hipertrofiando/cadastro/TelaApresentacao.java
@@ -0,0 +1,18 @@
+package br.com.michelsoares.hipertrofiando.cadastro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import br.com.michelsoares.hipertrofiando.R;
+
+/**
+ * Created by Michel on 18/08/2015.
+ */
+public class TelaApresentacao extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_apresentacao);
+    }
+}
